4bb58b450c553f04fe427b337623946c09716461
FelipePardim <felipesantos.2017@alunos.utfpr.edu.br>
2020-11-11 11:35:54 -0300

    Multiple hits hutbox fix

--- Hurtboxes + Hitboxes/Hurtbox.gd
+++ Hurtboxes + Hitboxes/Hurtbox.gd
@@ -5,6 +5,7 @@ const HitEffect = preload("res://Effects/HitEffect.tscn")
 var invincible = false setget set_invincible
 
 onready var timer = $Timer
+onready var collisionShape = $CollisionShape2D
 
 signal invincibility_started
 signal invincibility_ended
@@ -30,8 +31,7 @@ func _on_Timer_timeout():
 	self.invincible = false
 
 func _on_Hurtbox_invincibility_ended():
-	monitorable = true
+	collisionShape.disabled = false
 
 func _on_Hurtbox_invincibility_started():
-	set_deferred("monitorable", false)
-	monitorable = false
+	collisionShape.set_deferred("disabled", true)
